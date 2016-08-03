32c9e98e2158869dc7a2c915c39441a921f6949a
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-02 21:13:17 -0700

    update layout filter

--- app/models/naturesoft/slideshows/slide.rb
+++ app/models/naturesoft/slideshows/slide.rb
@@ -1,5 +1,18 @@
 module Naturesoft::Slideshows
   class Slide < ApplicationRecord
     belongs_to :user
+    
+    def self.sort_by
+      [
+        ["Name","name"],
+        ["Created At","created_at"]
+      ]
+    end
+    def self.sort_orders
+      [
+        ["ASC","asc"],
+        ["DESC","desc"]
+      ]
+    end
   end
 end
--- app/models/naturesoft/slideshows/slideshow.rb
+++ app/models/naturesoft/slideshows/slideshow.rb
@@ -2,13 +2,25 @@ module Naturesoft::Slideshows
   class Slideshow < ApplicationRecord
     belongs_to :user
     
-    def self.image_style
-    [
-      #[I18n.t('FILL'),"fill"],
-      #[I18n.t('FIT'),"fit"]
-      ["FILL","fill"],
-      ["FIT","fit"]
-    ]
-  end
+    def self.filter_image_style
+      [
+        ["FILL","fill"],
+        ["FIT","fit"],
+      ]
+    end
+    def self.sort_by
+      [
+        ["Name","name"],
+        ["Height","height"],
+        ["Width","width"],
+        ["Created At","created_at"]
+      ]
+    end
+    def self.sort_orders
+      [
+        ["ASC","asc"],
+        ["DESC","desc"]
+      ]
+    end
   end
 end
