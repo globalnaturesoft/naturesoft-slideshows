2b1e8585727052433048ba2338f964bf74327194
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-11 18:47:39 -0700

    update module slideshow for website

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,10 @@
+Naturesoft::Nsmodule.class_eval do
+  @slideshows = {
+    "slider" => {
+      "label" => "Slider",
+      "options" => {
+        "slideshow_id" => nil,
+      }    
+    }
+  }
+end
